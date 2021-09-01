91e2bb249a098403587881c65592062f8af1b40a
gabrielbernalle <gabrielbernalle@gmail.com>
2021-08-31 22:11:26 -0300

    Começando trabalhos de rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+    const { title } = request.query;
+
+    console.log(title);
+    
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -12,6 +18,10 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) =>{
+    const body = request.body;
+
+    console.log(body);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -20,14 +30,18 @@ app.post('/projects', (request, response) =>{
     ]);
 });
 
-app.put('/projects', (request, response)=>{
+app.put('/projects/:id', (request, response)=>{
+    const { id } = request.params;
+    
+    console.log(id);
+
     return response.json([
         'Projeto 5',
         'Projeto 2',
         'Projeto 3',
         'Projeto 4'
     ]);
-});
+}); 
 
 app.delete('/projects',(request,response)=>{
     return response.json([
