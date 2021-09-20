3637681cb4755fa69b07eede3f698c82e9ba39dc
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2021-09-20 18:51:09 -0300

    usando o seed com kenex

--- backend_TypeScript/src/server.ts
+++ backend_TypeScript/src/server.ts
@@ -1,9 +1,12 @@
 import express, { request, response } from 'express';
+import path from 'path'
 import routes from './routes';
 
 const app = express();
 
 app.use(routes);   
+
+app.use('/uploads', express.static(path.resolve(__dirname,'..', 'uploads')));
 
 app.listen(8082, () => {
     console.log("Server started on port 8082 !!");
