d6390686a66aff3ed6cf9bfad9459ed0e875f8a7
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2021-10-20 19:03:27 -0300

    Fim do miniCurso Typescript

--- backend_TypeScript/src/routes/locations.routes.ts
+++ backend_TypeScript/src/routes/locations.routes.ts
@@ -1,5 +1,6 @@
 import { request, response, Router } from "express";
 import multer from "multer";
+import { celebrate,Joi } from "celebrate";
 import knex from "../database/connection";
 import multerConfig from '../config/multer';
 
@@ -47,7 +48,20 @@ locationsRouter.get('/:id', async (request, response) => {
     return response.json({ location, items });
 });
 
-locationsRouter.post('/', async (request, response) => {
+locationsRouter.post('/',celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required()
+    })
+},{
+    abortEarly:false
+}) ,async (request, response) => {
     const {
         name,
         email,
--- backend_TypeScript/src/server.ts
+++ backend_TypeScript/src/server.ts
@@ -1,6 +1,7 @@
 import express, { request, response } from 'express';
-import cors from 'cors'
-import path from 'path'
+import cors from 'cors';
+import path from 'path';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(routes);   
 
 app.use('/uploads', express.static(path.resolve(__dirname,'..', 'uploads')));
+
+app.use(errors());
 
 app.listen(8082, () => {
     console.log("Server started on port 8082 !!"); 
